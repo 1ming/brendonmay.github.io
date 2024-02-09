3415b43d6cdb68f1aaef3de438aeae9e0a7e647b
Ming Zhang <w98zhang@gmail.com>
2024-02-09 11:07:38 -0800

    switch to using semicolons

--- flameCalculator/mingcalc.js
+++ flameCalculator/mingcalc.js
@@ -2,7 +2,7 @@
 
 // probability for how many lines are "drawn" in total
 // for flame advantaged items, the number of lines is always 4
-const non_advantaged = { 1: 0.40, 2: 0.40, 3: 0.15, 4: 0.05 }
+const non_advantaged = { 1: 0.40, 2: 0.40, 3: 0.15, 4: 0.05 };
 
 // for non-flame advantaged items, each tier is reduced by 2 (e.g. tier 3 becomes tier 1)
 const TIER_PROBABILITIES = {
@@ -12,10 +12,10 @@ const TIER_PROBABILITIES = {
   fusion: { 3: 0.5, 4: 0.4, 5: 0.1, 6: 0, 7: 0 },
   masterFusion: { 3: 0.25, 4: 0.35, 5: 0.3, 6: 0.1, 7: 0 },
   meisterFusion: { 3: 0, 4: 0.4, 5: 0.45, 6: 0.14, 7: 0.01 },
-}
+};
 
 // these values will get altered by user input on the website
-var stat_equivalences = { "all_stat": 12, "secondary_stat": 0.066667, "attack": 3, "dmg": 15 }
+var stat_equivalences = { "all_stat": 12, "secondary_stat": 0.066667, "attack": 3, "dmg": 15 };
 
 var stat_per_tier = {
   "120-139": 7,
@@ -25,18 +25,18 @@ var stat_per_tier = {
   "200-229": 11,
   "230-249": 12,
   "250+": 12,
-}
+};
 
 // these are for weapons only
 var watt_per_tier_adv = { //as a percentage of base
   "160-199": { 3: 0.15, 4: 0.22, 5: 0.3025, 6: 0.3993, 7: 0.512435 },
   "200+": { 3: 0.18, 4: 0.264, 5: 0.363, 6: 0.47916, 7: 0.614922 }
-}
+};
 
 var watt_per_tier_non_adv = { //as a percentage of base
   "160-199": { 1: 0.05, 2: 0.11, 3: 0.185, 4: 0.2662, 5: 0.366025, 6: 0.43923, 7: 0.512435 },
   "200+": { 1: 0.06, 2: 0.132, 3: 0.2178, 4: 0.31944, 5: 0.43923, 6: 0.527076, 7: 0.614922 }
-}
+};
 
 // for armour: weapon or magic attack is 1:1 the tier value regardless of level
 
@@ -48,7 +48,7 @@ let combo_stat_per_tier = {
   "200-229": 6,
   "230-249": 6,
   "250+": 7,
-}
+};
 
 let hp_stat_per_tier = {
   "120-129": 360,
@@ -65,10 +65,10 @@ let hp_stat_per_tier = {
   "230-239": 660,
   "240-249": 680,
   "250+": 700,
-}
+};
 
 
-const NUM_LINE_TYPES = 19
+const NUM_LINE_TYPES = 19;
 
 // categorize lines based on how they will affect flame score calculations
 // for example, most classes will have:
@@ -82,13 +82,13 @@ const LINETYPE = {
   ALLSTAT: "All Stat %",
   COMBO_XENON_DOUBLE: "Xenon Main/Main",
   ATTACK: "Attack",
-}
+};
 
 const COMBO_LINES = [
   LINETYPE.COMBO_MAIN_SECOND,
   LINETYPE.COMBO_MAIN_JUNK,
   LINETYPE.COMBO_SECOND_JUNK,
-]
+];
 
 // map a function to calculate flame score for each line type
 const FLAME_SCORE = {
@@ -100,12 +100,12 @@ const FLAME_SCORE = {
   [LINETYPE.COMBO_SECOND_JUNK]: (value) => stat_equivalences.secondary_stat * value,
   [LINETYPE.ALLSTAT]: (value) => value * stat_equivalences.all_stat,
   [LINETYPE.ATTACK]: (value) => value * stat_equivalences.attack,
-}
+};
 
 const CLASS_TYPE = {
   NORMAL: "Normal",
   XENON: "Xenon",
-}
+};
 
 // the quantity of each line type that contributes to the flame score for a class
 // used to generate the pool of valid (non-junk) lines
@@ -118,82 +118,82 @@ const CLASS_LINES = {
     [LINETYPE.MAIN_STAT]: 3, [LINETYPE.COMBO_XENON_DOUBLE]: 3, [LINETYPE.COMBO_MAIN_JUNK]: 3,
     [LINETYPE.XENON_ALLSTAT]: 1, [LINETYPE.ATTACK]: 1,
   }
-}
+};
 
 
 // check if a level number is within a level range string
 // data for value per tier has a level range string as key (e.g. "140-149")
 function is_in_level_range(level_range_str, level_num) {
   if (level_range_str.endsWith("+")) {
-    return level_num >= Number(level_range_str.replace("+", ""))
+    return level_num >= Number(level_range_str.replace("+", ""));
   }
 
-  const level_ranges = level_range_str.split("-")
-  const low = Number(level_ranges[0])
-  const high = Number(level_ranges[1])
-  return level_num >= low && level_num <= high
+  const level_ranges = level_range_str.split("-");
+  const low = Number(level_ranges[0]);
+  const high = Number(level_ranges[1]);
+  return level_num >= low && level_num <= high;
 }
 
 // remove a line from the pool by key
-function update_pool(pool, key){
-  const new_pool = []
+function update_pool(pool, key) {
+  const new_pool = [];
   for (const item in pool) {
-    if (item != key) {
-      new_pool.push(pool[item])
+    if (item !== key) {
+      new_pool.push(pool[item]);
     }
   }
-  return new_pool
+  return new_pool;
 }
 
 // calculate the probability that the target will be met within num_draws total lines
 // num_draws: total number of lines we can "draw"/pick out of the pool, once
 // pool: list of valid lines that have not yet been drawn
 function get_p_recursive(line, target, pool, num_junk, num_draws, debug_data, parents) {
-  debug_data.count++
-  if (num_draws == 0 || pool.length == 0) {
-    return 0
+  debug_data.count++;
+  if (num_draws === 0 || pool.length === 0) {
+    return 0;
   }
 
-  const num_remaining_items = pool.length + num_junk
-  let p = 0
+  const num_remaining_items = pool.length + num_junk;
+  let p = 0;
 
-  parents.push(line != null ? line.name : "null")
+  parents.push(line != null ? line.name : "null");
 
   if (line != null) {
     for (const tier in line.tiers) {
       if (line.tiers[tier].score >= target) {
-        p += line.tiers[tier].p
-        debug_data.lines_picked.push(line.name + " (finished), " + tier + ", " + target)
-        debug_data.paths.push(parents)
+        p += line.tiers[tier].p;
+        debug_data.lines_picked.push(line.name + " (finished), " + tier + ", " + target);
+        debug_data.paths.push(parents);
       }
       else {
-        const new_target = target - line.tiers[tier].score
-        debug_data.lines_picked.push(line.name + " (partial), " + tier + ", " + target)
+        const new_target = target - line.tiers[tier].score;
+        debug_data.lines_picked.push(line.name + " (partial), " + tier + ", " + target);
 
         // recurse on all other lines in pool
         for (const succ_line_key in pool) {
           // remove this item from the pool for the next round
-          const new_pool = update_pool(pool, succ_line_key)
-          p += (1 / num_remaining_items) * get_p_recursive(pool[succ_line_key], new_target, new_pool, num_junk, num_draws - 1, debug_data, parents)
+          const new_pool = update_pool(pool, succ_line_key);
+          p += (1 / num_remaining_items) * get_p_recursive(pool[succ_line_key], new_target, new_pool, num_junk, num_draws - 1, debug_data, parents);
         }
       }
     }
   }
-  else{
+  else {
     // recurse on all valid lines without changing the target
     for (const succ_line_key in pool) {
       // remove the  item from the pool for the next round
-      const new_pool = update_pool(pool, succ_line_key)
-      p += (1 / num_remaining_items) * get_p_recursive(pool[succ_line_key], target, new_pool, num_junk, num_draws - 1, debug_data, parents)
+      const new_pool = update_pool(pool, succ_line_key);
+      p += (1 / num_remaining_items) * get_p_recursive(pool[succ_line_key], target, new_pool, num_junk, num_draws - 1, debug_data, parents);
     }
   }
 
   if (pool.length > 0 && num_junk > 0) {
     // recurse on all junk lines (lumped)
-    p += (num_junk / num_remaining_items) * get_p_recursive(null, target, pool, num_junk - 1, num_draws - 1, debug_data, parents)
+    p += (num_junk / num_remaining_items) * get_p_recursive(null, target, pool, num_junk - 1, num_draws - 1, debug_data, parents);
   }
 
-  return p
+  return p;
 
 }
 
@@ -203,22 +203,22 @@ function get_p_recursive(line, target, pool, num_junk, num_draws, debug_data, pa
 function get_tier_value(line_type, tier, level, is_adv, base_att) {
   if (base_att != null) {
     // weapon flame changes based on flame advantage and base attack
-    const perc_per_tier = is_adv ? watt_per_tier_adv : watt_per_tier_non_adv
+    const perc_per_tier = is_adv ? watt_per_tier_adv : watt_per_tier_non_adv;
     for (const level_range_str in perc_per_tier) {
       if (is_in_level_range(level_range_str, level)) {
-        return base_att * perc_per_tier[level_range_str][tier]
+        return base_att * perc_per_tier[level_range_str][tier];
       }
     }
   }
   else if (line_type === LINETYPE.ALLSTAT || line_type === LINETYPE.ATTACK) {
     // 1% all stat per tier, 1 att per tier for non-weapons
-    return tier
+    return tier;
   }
   else {
-    const stat_tier_dict = COMBO_LINES.includes(line_type) ? combo_stat_per_tier : stat_per_tier
+    const stat_tier_dict = COMBO_LINES.includes(line_type) ? combo_stat_per_tier : stat_per_tier;
     for (const level_range_str in stat_tier_dict) {
       if (is_in_level_range(level_range_str, level)) {
-        return tier * stat_tier_dict[level_range_str]
+        return tier * stat_tier_dict[level_range_str];
       }
     }
   }
@@ -234,44 +234,44 @@ function getLineData(line_type, level, is_adv, flame_type, class_type) {
     name: line_type,
     f_max: 0,
     tiers: {},
-  }
+  };
 
   // get the active tiers and corresponding probability based on the type of flame used
   // for each tier, determine what the value is based on the line type and the character level
   // also calculate the flame score based on class type
   for (const key in TIER_PROBABILITIES[flame_type]) {
     // all tiers are reduced by 2 for non-flame advantaged items
-    const tier = is_adv ? key : key - 2
-    const p_tier = TIER_PROBABILITIES[flame_type][key]
+    const tier = is_adv ? key : key - 2;
+    const p_tier = TIER_PROBABILITIES[flame_type][key];
     if (p_tier === 0) {
       // don't include tiers that have no chance of appearing
-      continue
+      continue;
     }
 
-    const tier_value = get_tier_value(line_type, tier, level, is_adv, null)
+    const tier_value = get_tier_value(line_type, tier, level, is_adv, null);
     line.tiers[tier] = {
       p: p_tier,
       value: tier_value,
       score: FLAME_SCORE[line_type](tier_value)
-    }
+    };
 
     // update the maximum possible flame score we can get from this line (highest tier)
     if (line.tiers[tier].score > line.f_max) {
-      line.f_max = line.tiers[tier].score
+      line.f_max = line.tiers[tier].score;
     }
   }
 
-  return line
+  return line;
 }
 
 // calculate the probability of obtaining a target flame score
 function getProbability(class_type, level, flame_type, is_adv) {
   // generate pool of valid line objects containing attributes such as:
   // tiers, values, probabilities which are based on flame type used and level
-  const valid_lines = []
+  const valid_lines = [];
   for (const key in CLASS_LINES[class_type]) {
     for (let i = 0; i < CLASS_LINES[class_type][key]; i++) {
-      valid_lines.push(getLineData(key, level, is_adv, flame_type, class_type))
+      valid_lines.push(getLineData(key, level, is_adv, flame_type, class_type));
     }
   }
 
@@ -280,16 +280,16 @@ function getProbability(class_type, level, flame_type, is_adv) {
     count: 0,
     lines_picked: [],
     paths: []
-  }
-  const result = get_p_recursive(null, 20, valid_lines, NUM_LINE_TYPES - valid_lines.length, 2, debug_data, [])
+  };
+  const result = get_p_recursive(null, 20, valid_lines, NUM_LINE_TYPES - valid_lines.length, 2, debug_data, []);
 
-  return result
+  return result;
 }
 
 // for testing
-const class_type = CLASS_TYPE.NORMAL
-const level = 150
-const flame_type = "powerful"
-const is_adv = true
+const class_type = CLASS_TYPE.NORMAL;
+const level = 150;
+const flame_type = "powerful";
+const is_adv = true;
 
-getProbability(class_type, level, flame_type, is_adv)
+getProbability(class_type, level, flame_type, is_adv);
